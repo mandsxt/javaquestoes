67168e3f88f0ee2f44b837ac355fd608672e1dbf
mandsxt <36248355+mandsxt@users.noreply.github.com>
2018-02-28 19:07:44 -0300

    Update RespostaQ7.java

--- RespostaQ7.java
+++ RespostaQ7.java
@@ -1,4 +1,44 @@
+import java.util.Scanner;
 
 public class RespostaQ7 {
-
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		float n1;
+		float n2;
+		float[] me=new float[6];
+		float met=0;
+		int ap=0,re=0,fn=0;
+		for(int i=0;i<6;i++) {
+			System.out.println("1º nota:");
+			n1=input.nextFloat();
+			System.out.println("2º nota:");
+			n2=input.nextFloat();
+			me[i]=(n1+n2)/2;
+			if(me[i]<3) {
+				re=re+1;
+			}else if(me[i]<7){
+				fn=fn+1;
+			}else {
+				ap=ap+1;
+			}
+		}
+		input.close();
+		for(int i=0;i<6;i++) {
+			System.out.println("Aluno"+(i+1)+" media= "+me[i]);
+			met=me[i]+met;
+			if(me[i]<3) {
+				System.out.println("Reprovado");
+			}else if(me[i]<7){
+				System.out.println("Final");
+			}else {
+				System.out.println("Aprovado");
+			}
+		}
+		met=met/6;
+		System.out.println("Media da classe="+met);
+		System.out.println("Aprovados="+ap);
+		System.out.println("Final="+fn);
+		System.out.println("Reprovados="+re);
+	}
+}
 }
